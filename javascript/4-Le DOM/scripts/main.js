63da3c44f6a42431fd0b9e31456b4d92fcc5ec3b
Nolwein Delsaut <delsaut.nolwein@gmail.com>
2023-11-10 13:46:33 +0100

    update

---
+++ javascript/4-Le DOM/scripts/main.js
@@ -0,0 +1,13 @@
+lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture");
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementByID("btnValiderMot");
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zoneProposition");
+
+let zoneScore = document.querySelector(".zoneScore span");
+
+let optionSource = document.querySelector(".optionSource input");
