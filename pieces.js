da74f1dd9f6ba314dc4917a218541f7cd9278b49
Nolwein Delsaut <delsaut.nolwein@gmail.com>
2023-11-23 13:02:52 +0100

    boucles

---
+++ pieces.js
@@ -0,0 +1,38 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+for (let i = 0; i < pieces.length; i++) {
+
+  const article = pieces[i];
+
+  const pieceElement = document.createElement("article");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = article.image;
+
+  const nomElement = document.createElement("h2");
+  nomElement.innerText = article.nom;
+
+  const prixElement = document.createElement("p");
+  prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
+  const categorieElement = document.createElement("p");
+  categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText = article.description ?? "(pas de description pour le moment)";
+
+  const disponibiliteElement = document.createElement("p");
+  disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+  const sectionFiches = document.querySelector(".fiches");
+  sectionFiches.appendChild(pieceElement);
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(disponibiliteElement);
+
+}
