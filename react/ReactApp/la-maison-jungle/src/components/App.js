b850bdaa67ddc7b587dd953834c4c73a33d35690
Nolwein Delsaut <delsaut.nolwein@gmail.com>
2023-12-15 21:03:19 +0100

    state

--- react/ReactApp/la-maison-jungle/src/components/App.js
+++ react/ReactApp/la-maison-jungle/src/components/App.js
@@ -1,6 +1,6 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
@@ -11,8 +11,7 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-
-			{/* <Cart /> */}
+			<Cart />
 			<ShoppingList />
 			<Footer />
 		</div>
